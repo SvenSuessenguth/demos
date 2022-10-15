43762cc8b88fcfe8371a2015e3362c3211e018b3
SvenSuessenguth <familie.suessenguth@googlemail.com>
2022-10-15 07:06:38 +0200

    Entfernen unbenutzter imports

--- mp-rest-client/src/test/java/org/cc/samples/mprestclient/AgifyServiceTest.java
+++ mp-rest-client/src/test/java/org/cc/samples/mprestclient/AgifyServiceTest.java
@@ -6,7 +6,6 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
