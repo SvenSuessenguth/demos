96c7c5764abd61e386df6b9f439adde59c7ca0cb
SvenSuessenguth <familie.suessenguth@googlemail.com>
2023-04-16 13:35:28 +0200

    clear

--- mp-rest-client/src/test/java/org/cc/samples/mprestclient/AgifyApplication.java
+++
@@ -1,8 +0,0 @@
-package org.cc.samples.mprestclient;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class AgifyApplication extends Application {
-}
--- weld-junit5-demo/src/main/java/ApplicationScopedBean.java
+++
@@ -1,7 +0,0 @@
-import jakarta.enterprise.context.ApplicationScoped;
-import java.io.Serializable;
-
-@ApplicationScoped
-public class ApplicationScopedBean implements Serializable {
-
-}
